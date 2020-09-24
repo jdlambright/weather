de68c023479c16bae1695ccd446377542e385627
jdlambright <64182772+jdlambright@users.noreply.github.com>
2020-09-24 17:05:56 -0400

    finalized page to received input and get weather

--- app.js
+++ app.js
@@ -11,32 +11,33 @@ app.get("/", function(req, res){
 
 });
 
-app.post("/", function(){
-  console.log("it works")
+app.post("/", function(req, res){
+  const query = req.body.cityName;
+  const apiKey = "843da59170b3338133b92d6d1296ef83";
+  const units = "imperial";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey+ "&units="+ units;
+  https.get( url, function(response){
+    console.log(response.statusCode);
+
+    response.on("data", function(data){
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const description = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const imageURL ="http://openweathermap.org/img/wn/" + icon + "@2x.png"
+
+    res.write("<h1>The temperature in " + query + " is " + temp + " degrees</h1>")
+    res.write("<p>The current conditions are " + description + "</p>")
+    res.write("<img src="+imageURL+">");
+    res.send()
+    console.log(temp);
+    console.log(description);
+    })
+  })
+
 })
 
-//const query = "London";
-//const apiKey = "843da59170b3338133b92d6d1296ef83";
-//const units = "imperial";
-//const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey+ "&units="+ units;
-//https.get( url, function(response){
-  //console.log(response.statusCode);
 
-  //response.on("data", function(data){
-    //const weatherData = JSON.parse(data)
-    //const temp = weatherData.main.temp
-    //const description = weatherData.weather[0].description
-    //const icon = weatherData.weather[0].icon
-    //const imageURL ="http://openweathermap.org/img/wn/" + icon + "@2x.png"
-
-  //  res.write("<h1>The temperature in London is " + temp + " degrees</h1>")
-    //res.write("<p>The current conditions are " + description + "</p>")
-    //res.write("<img src="+imageURL+">");
-  //  res.send()
-    //console.log(temp);
-    //console.log(description);
-  //})
-//})
 
 
 app.listen(3000, function(){
